f33eeff2294a9ea14611cd68f688ebdad59256b7
slavashestakov2005 <slavashestakov2005@gmail.com>
2024-12-23 19:30:51 +0300

    Начало второй версии

--- bot/analyzer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <board/board.h>
-
-class Analyzer {
-public:
-    static int analyzeMaterial(Board const& board);
-    static int analyzeMaterialPosition(Board const& board);
-};
--- figures/all.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <figures/bishop.h>
-#include <figures/empty.h>
-#include <figures/king.h>
-#include <figures/knight.h>
-#include <figures/pawn.h>
-#include <figures/queen.h>
-#include <figures/rook.h>
